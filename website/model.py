cf00947773a0de6bfe93bdbaea5709a384bf7722
justin.forest <justin.forest@localhost>
2009-12-06 19:29:28 +0300

    ?????? ???????????? ??????.

--- website/model.py
+++ website/model.py
@@ -1,32 +1,39 @@
 # -*- coding: utf-8 -*-
-# vim: set ts=2 sts=2 sw=2 et:
+# vim: set ts=4 sts=4 sw=4 noet:
 
+import urllib
 from google.appengine.ext import db
 
 class SiteUser(db.Model):
-  user = db.UserProperty()
-  joined = db.DateTimeProperty(auto_now_add=True)
-  weight = db.FloatProperty()
+	user = db.UserProperty()
+	joined = db.DateTimeProperty(auto_now_add=True)
+	weight = db.FloatProperty()
 
 class SiteArtist(db.Model):
-  name = db.StringProperty(required=True)
+	name = db.StringProperty(required=True)
 
 class SiteTrack(db.Model):
-  name = db.StringProperty()
-  number = db.IntegerProperty()
-  mp3_link = db.LinkProperty()
+	title = db.StringProperty()
+	artist = db.ReferenceProperty(SiteArtist) # for compilations
+	lyrics = db.TextProperty()
+	number = db.IntegerProperty()
+	mp3_link = db.LinkProperty()
+
+	def getXML(self):
+		return '<track title="%s" artist="%s" number="%s" mp3="%s"/>' % (self.title,
+			self.artist.name, self.number, self.mp3_link)
 
 class SiteAlbum(db.Model):
-  name = db.StringProperty()
-  artist = db.ReferenceProperty(SiteArtist)
-  release_date = db.DateTimeProperty(auto_now_add=True)
-  rating = db.RatingProperty() # average album rate
-  cover_small = db.LinkProperty() # image URL
-  cover_large = db.LinkProperty() # image URL
-  tracks = db.BlobProperty()
-  files = db.BlobProperty()
+	name = db.StringProperty()
+	artist = db.ReferenceProperty(SiteArtist)
+	release_date = db.DateTimeProperty(auto_now_add=True)
+	rating = db.RatingProperty() # average album rate
+	cover_small = db.LinkProperty() # image URL
+	cover_large = db.LinkProperty() # image URL
+	tracks = db.BlobProperty()
+	files = db.BlobProperty()
 
 class SiteAlbumReview(db.Model):
-  album = db.ReferenceProperty(SiteAlbum)
-  author = db.ReferenceProperty(SiteUser)
-  created = db.DateTimeProperty(auto_now_add=True)
+	album = db.ReferenceProperty(SiteAlbum)
+	author = db.ReferenceProperty(SiteUser)
+	created = db.DateTimeProperty(auto_now_add=True)
---
+++ website/static/style.xsl
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- vim: set ts=4 sts=4 sw=4 noet: -->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output
+		omit-xml-declaration="yes"
+		method="xml"
+		version="1.0"
+		encoding="utf-8"
+		indent="no"
+		doctype-public="-//W3C//DTD XHTML 1.0 Transitional//EN"
+		doctype-system="http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"
+		/>
+	<xsl:template match="/page">
+		<html lang="{@lang}">
+			<body>
+				<xsl:apply-templates select="*"/>
+			</body>
+		</html>
+	</xsl:template>
+
+	<xsl:template match="/page/album">
+		<h1>
+			<xsl:value-of select="@name"/>
+		</h1>
+		<xsl:if test="@pubDate">
+			<p>Published on <xsl:value-of select="@pubDate"/></p>
+		</xsl:if>
+		<xsl:apply-templates select="cover"/>
+		<xsl:apply-templates select="tracks"/>
+		<xsl:apply-templates select="files"/>
+	</xsl:template>
+
+	<xsl:template match="cover">
+		<img src="{@uri}" alt="cover"/>
+	</xsl:template>
+
+	<xsl:template match="tracks">
+		<h2>Tracks</h2>
+		<ul>
+			<xsl:apply-templates select="track"/>
+		</ul>
+	</xsl:template>
+
+	<xsl:template match="track">
+		<li>
+			<a href="{@mp3}">
+				<xsl:value-of select="@title"/>
+			</a>
+		</li>
+	</xsl:template>
+
+	<xsl:template match="files">
+		<h2><a href="{file/@uri}">Download</a></h2>
+		<ul>
+			<xsl:for-each select="file">
+				<li>
+					<a href="{@uri}">
+						<xsl:value-of select="@name"/>
+					</a>
+				</li>
+			</xsl:for-each>
+		</ul>
+	</xsl:template>
+</xsl:stylesheet>
