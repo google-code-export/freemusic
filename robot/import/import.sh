d7d974e683395ddb9ae86849c06bfbd96470f448
justin.forest <justin.forest@localhost>
2009-12-26 03:14:15 +0300

    ????????? ^C ?? ????? ???????.

--- robot/import/import.sh
+++ robot/import/import.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+set -u
 cd $(dirname $0)
 rm -f artist-*.xml
+trap "rm -f *.xml; exit 1" INT
+echo "Downloading from ebm.net.ru..."
 wget -qO import.xml 'http://ebm.net.ru/api/node/list.xml?class=release,file,artist&limit=none' \
   && xsltproc import.xsl import.xml > data.xml
+echo "Uploading to AppEngine..."
 for f in album-*.xml; do
-  echo "Processing $f..."
   ../robot.py -fa $f
 done
 rm -f album-*.xml
