48c3b29b0737220997607427c0559ae938352d38
justin.forest <justin.forest@localhost>
2010-01-09 16:00:47 +0300

    ?????????? ????????? ? lib/zip.py

--- robot/lib/zip.py
+++ robot/lib/zip.py
@@ -7,41 +7,41 @@ import os
 import zipfile
 
 def unzip(zipname, tmpdir):
-    """
-    Распаковывает указанный ZIP архив во временную папку,
-    возвращает список с именами файлов.
-    """
-    logger.info("unzipping " + zipname)
-    result = []
-    try:
-        zip = zipfile.ZipFile(zipname)
-    except zipfile.BadZipfile, e:
-        logger.error(str(e))
-        return []
-    for f in sorted(zip.namelist()):
-        if not f.endswith('/'):
-            name = os.path.basename(f)
-            try:
-                name = name.decode('utf-8')
-            except UnicodeDecodeError:
-                parts = os.path.splitext(name)
-                name = urllib.quote(parts[0]).replace('%', 'x') + parts[1]
-            if name.startswith('.'):
-                logger.info(u'- ' + name)
-            else:
-                result.append(os.path.join(unicode(tmpdir), name))
-                out = open(result[-1], 'wb')
-                out.write(zip.read(f))
-                out.close()
-                logger.info(u"< " + name)
-    return result
+	"""
+	Распаковывает указанный ZIP архив во временную папку,
+	возвращает список с именами файлов.
+	"""
+	logger.info("unzipping " + zipname)
+	result = []
+	try:
+		zip = zipfile.ZipFile(zipname)
+	except zipfile.BadZipfile, e:
+		logger.error(str(e))
+		return []
+	for f in sorted(zip.namelist()):
+		if not f.endswith('/'):
+			name = os.path.basename(f)
+			try:
+				name = name.decode('utf-8')
+			except UnicodeDecodeError:
+				parts = os.path.splitext(name)
+				name = urllib.quote(parts[0]).replace('%', 'x') + parts[1]
+			if name.startswith('.'):
+				logger.debug(u'- ' + name)
+			else:
+				result.append(os.path.join(unicode(tmpdir), name))
+				out = open(result[-1], 'wb')
+				out.write(zip.read(f))
+				out.close()
+				logger.info(u"< " + name)
+	return result
 
 def zip(filename, files):
-    logger.info(u'creating ' + filename)
-    z = zipfile.ZipFile(filename, 'a')
-    for file in files:
-        if not file.endswith('.zip'):
-            logger.info(u"+ " + file)
-            z.write(file, os.path.basename(file))
+	logger.info(u'creating ' + filename)
+	z = zipfile.ZipFile(filename, 'w')
+	for file in files:
+		if not file.endswith('.zip'):
+			logger.info(u"+ " + file)
+			z.write(file, os.path.basename(file))
 	z.close()
 	return filename
