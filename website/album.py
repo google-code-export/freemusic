87531748ec52d3e77647a2bb3a35f56f11d0c804
justin.forest <justin.forest@localhost>
2010-01-12 15:29:54 +0300

    ????? ??????????????? ???????.

--- website/album.py
+++ website/album.py
@@ -34,20 +34,39 @@ class Editor(Viewer):
 		self.sendXML('<form>' + self.get_album(id).xml + '</form>')
 
 	def post(self, id):
-		log(self.request.arguments())
+		try:
+			album = self.get_album(id)
+			self.force_user_or_admin(album.owner)
 
-		album = self.get_album(id)
-		self.force_user_or_admin(album.owner)
+			album.name = self.request.get(u'name')
+			album.release_date = datetime.datetime.strptime(self.request.get(u'pubDate'), '%Y-%m-%d').date()
+			album.text = self.request.get(u'text')
+			album.labels = [unicode(label.strip()).lower() for label in self.request.get('labels').split(',')]
 
-		album.name = self.request.get(u'name')
-		album.release_date = datetime.datetime.strptime(self.request.get(u'pubDate'), '%Y-%m-%d').date()
-		album.text = self.request.get(u'text')
-		album.labels = [unicode(label.strip()).lower() for label in self.request.get('labels').split(',')]
+			# Переименовываем дорожки
+			self.rename_tracks(album)
 
-		album.put()
-		album.artist.put() # обновление XML
+			album.put()
+			album.artist.put() # обновление XML
 
-		self.redirect('/album/' + str(album.id))
+			self.redirect('/album/' + str(album.id))
+		except Exception, e:
+			log([(arg, self.request.get(arg)) for arg in self.request.arguments()])
+			raise
+
+	def rename_tracks(self, album):
+		"Извлекает из запроса номера и имена дорожек, сохраняет их."
+		data = {}
+		for arg in self.request.arguments():
+			if arg.startswith('track.') and self.request.get(arg):
+				data[int(arg.split('.', 1)[1])] = self.request.get(arg)
+		if data:
+			log(data)
+			for track in SiteTrack.gql('WHERE album = :1', album).fetch(1000):
+				if track.number in data:
+					track.title = data[track.number]
+					track.put()
+
 
 class Delete(Viewer):
 	def get(self, id):
