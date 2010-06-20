dbd3e3d55b7c3589ff8431a3555ed5335a976a01
justin.forest <justin.forest@localhost>
2010-06-20 21:42:30 +0400

    ????????????? ??????? ????? ?????????????.

--- website/static/scripts/50albumplayer.js
+++ website/static/scripts/50albumplayer.js
@@ -1,28 +1,34 @@
 $(function(){
-	$('#album #tracklist tr .l a').click(function(){
-		if (Modernizr.audio) {
-			var row = $(this).parents('tr:first'), link, sel;
-			if (Modernizr.audio.ogg)
-				sel = '.ogg';
-			else if (Modernizr.audio.mp3)
-				sel = '.mp3';
-			if (sel) {
-				var link = row.find(sel).attr('href');
-				if (link) {
-					var player = $('#album audio')[0];
-					player.src = link;
-					player.play();
-					$(this).parents('table:first').find('tr').removeClass('np');
-					row.addClass('np');
-					return false;
-				}
+	var playrow = function (row) {
+		var sel;
+		if (Modernizr.audio.ogg)
+			sel = '.ogg';
+		else if (Modernizr.audio.mp3)
+			sel = '.mp3';
+		if (sel) {
+			var link = row.find(sel).attr('href');
+			if (link) {
+				var player = $('#album audio')[0];
+				player.src = link;
+				player.play();
+				row.parents('table:first').find('tr').removeClass('np');
+				row.addClass('np');
+				return true;
 			}
 		}
+	}
+
+	$('#album #tracklist tr .l a').click(function(){
+		if (Modernizr.audio)
+			return !playrow($(this).parents('tr:first'));
 	});
 
 	$('html.audio #player audio').each(function(){
 		$(this).bind('ended', function () {
-			$('#tracklist tr').removeClass('np');
+			var next = $('#tracklist tr.np ~ tr');
+			if (!next.length)
+				next = $('#tracklist tr:first');
+			playrow(next);
 		});
 	});
 });
