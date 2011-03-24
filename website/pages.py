449a9576efe2ee57cb28b4faaaf17ec1cc2e3e87
justin.forest <justin.forest@localhost>
2011-03-24 16:19:03 +0300

    More basic templates.

--- website/pages.py
+++ website/pages.py
@@ -28,7 +28,7 @@ textarea { width: 100%; height: 200px; white-space: nowrap; overflow: auto }
 </style>
 </head>
 <body>
-<h1>Edit page "<a href="{{ page_path|escape }}">{{ page.name }}</a>"</h1>
+<h1>Редактирование страницы «<a href="{{ page_path|escape }}">{{ page.name }}</a>»</h1>
 <form method="post">
 <input type="hidden" name="key" value="{% if page.is_saved %}{{ page.key }}{% endif %}"/>
 <div><label><span>Name:</span> <input type="text" name="name" value="{% if page.name %}{{ page.name }}{% endif %}"/></label></div>
@@ -47,9 +47,53 @@ DEFAULT_TEMPLATE = u'''<html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 <title>{% block title %}{% if title %}{{ title }}{% else %}Untitled Page{% endif %}{% endblock %}</title>
+{% if favicon %}<link rel="shortcut icon" type="image/x-icon" href="{{ favicon|escape }}"/>{% endif %}
+{% block scripts %}
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+ <script type="text/javascript" src="/static/pages/jquery.jplayer.min.js"></script>
+ <script type="text/javascript" src="/static/pages/player.js"></script>
+{% endblock %}
+{% block styles %}
+ <link rel="stylesheet" type="text/css" href="/static/pages/skins/{% if skin %}{{ skin }}{% else %}default{% endif %}.css"/>{% if page.css %}
+ <style type="text/css">{{ page.css }}</style>{% endif %}
+{% endblock %}
 </head>
 <body>
-<h1>{% block header %}It Worked!{% endblock %}</h1>
+<div id="wrapper">
+ <div id="header">
+  {% block header %}<h1>{% block welcome %}{{ page.name }}{% if page.artist_name %} by {{ page.artist_name }}{% endif %}{% endblock %}</h1>{% endblock %}
+ </div>
+ <div id="body">
+  <div id="column1">{% block column1 %}
+   <div id="picture" class="box">
+    <h2>{% block picture_title %}Обложка{% endblock %}</h2>
+    <img alt="album art" src="{% if album_art %}{{ album_art|escape }}{% else %}/static/pages/art.jpg{% endif %}"/>
+   </div>
+  {% endblock %}</div>
+  <div id="column2">{% block column2 %}
+   <div id="player" class="box">
+    <h2>{% block player_title %}Послушать альбом{% endblock %}</h2>
+    <div id="inplayer" class="jpbox playlist:{% if playlist %}{{ playlist|escape }}{% else %}/static/pages/playlist.json{% endif %}">{% block player_wait_text %}Дождитесь загрузки проигрывателя.{% endblock %}</div>
+   </div><!-- #player -->
+    {% if download_link %}<div id="dlink">
+     <h2>{% block download_title %}Скачать альбом{% endblock %}</h2>
+     <p>{% block download_link %}<a href="{{ download_link|escape }}">Нажмите сюда</a> чтобы скачать альбом.{% endblock %}</p>
+    </div>{% endif %}
+  {% endblock %}</div><!-- #column2 -->
+ </div>
+ <div id="footer">{% block footer %}
+  {% if welcome %}<p>{{ welcome }}</p>{% endif %}
+    {% if google_group %}<div id="subscribe">
+     <h2>Новости группы</h2>
+     <p>Подпишитесь на новостную рассылку, чтобы узнавать о концертах, новых альбомах и других новостях (см. <a href="http://groups.google.com/group/{{ google_group|escape }}" class="external">архив рассылки</a>).</p>
+     <form action="http://groups.google.com/group/{{ google_group|escape }}/boxsubscribe">
+      <input type="text" name="email" placeholder="Ваш email"/>
+      <input type="submit" value="Подписаться"/>
+     </form>
+    </div>{% endif %}<!-- #subscribe -->
+  <div id="license"><a title="Ознакомиться с текстом лицензии" class="external" href="http://creativecommons.org/licenses/{% if license %}{{ license|escape }}{% else %}by-sa{% endif %}/3.0/" class="external"><img src="http://i.creativecommons.org/l/{% if license %}{{ license|escape }}{% else %}by-sa{% endif %}/3.0/80x15.png" style="width:80px; height:15px" alt="Creative Commons License"/></a></div>
+ {% endblock %}</div>
+</div>
 </body>
 </html>'''
 
@@ -105,6 +149,8 @@ class PageHandler(webapp.RequestHandler):
             'base_template': Template(DEFAULT_TEMPLATE),
             'page': page,
         }
+
+        variables.update(dict([[x.strip() for x in line.split(':', 1)] for line in page.data.split('\n') if ':' in line]))
 
         t = Template(template).render(Context(variables))
         return self.reply(t)
